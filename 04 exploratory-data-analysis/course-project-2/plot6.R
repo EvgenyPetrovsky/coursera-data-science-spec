ab6f47264c0b34bc87a49ec5798d6770a7763809
EvgenyPetrovsky <ev.petrovsky@gmail.com>
2017-01-27 22:34:05 +0100

    rethinking plot6

--- 04 exploratory-data-analysis/course-project-2/plot6.R
+++ 04 exploratory-data-analysis/course-project-2/plot6.R
@@ -7,6 +7,7 @@ library(ggplot2)
 NEI <- readRDS("./workdata/input/summarySCC_PM25.rds")
 SCC <- readRDS("./workdata/input/Source_Classification_Code.rds")
 
+# selecting all categories that belong to motor vehicle
 motor <- 
   tbl_df(SCC) %>%
   # contains VEHICLE
@@ -14,16 +15,17 @@ motor <-
   # if vehicle uses GASOLINE or DIESEL - it has motor
   filter(grepl(pattern = "gasoline|diesel", x = Short.Name, ignore.case = TRUE))
 
+# list of fips required for plot
 fipsList <- data.frame(
   fips = c("06037", "24510"),
-  State.Label = c("Los Angeles County", "Baltimore City")
+  County = c("Los Angeles County", "Baltimore City")
 )
 
 allYears <- 
   # converting to tbl_df object of dplyr library and chaining
   tbl_df(NEI) %>% 
   # filtering data for Baltimore City by fips = 24510
-  filter(fips %in% c("24510", "06037")) %>%
+  filter(fips %in% fipsList$fips) %>%
   # filtering data by motor vehicle related sources
   filter(SCC %in% motor$SCC) %>%
   # split data by year
@@ -33,18 +35,34 @@ allYears <-
   # join with fipsList in order to have fips labels
   inner_join(y = tbl_df(fipsList))
 
+# add 1999 year measurements values
+firstYear <- allYears %>% 
+  group_by(fips) %>% arrange(year) %>% slice(1) %>% ungroup %>%
+  select(fips, First.Meas = Emissions) %>%
+  inner_join(y = allYears, by = "fips")
+
+# extending data with changes over time comparing to initial value
+extended <- firstYear %>%
+  mutate(Change = Emissions - First.Meas) %>%
+  mutate(Change.in.Percent = (1 + (Emissions - First.Meas) / First.Meas) * 100) %>%
+  select(year, County, Emissions, Change, Change.in.Percent)
+
+# reshaping data - gathering columns into rows
+tidy <- tidyr::gather(extended, "Meas.Type", "Value", c(Emissions, Change, Change.in.Percent))
+# reordering Measurement Types for plot
+tidy$Meas.Type <- factor(tidy$Meas.Type, levels = c("Emissions", "Change", "Change.in.Percent"))
 
 # open png device
-png("./workdata/output/plot6.png", width = 800, height = 500)
+png("./workdata/output/plot6.png", width = 500, height = 800)
 
 # plotting
 plotObject <- 
   # initialize
-  ggplot(allYears, aes(x = year, y = Emissions)) + 
+  ggplot(tidy, aes(x = year, y = Value, group = County, color = County)) + 
   # add points for all years
-  geom_point() + 
+  geom_line() + 
   # break plot into facets by emission type
-  facet_grid(. ~ State.Label) + 
+  facet_grid(Meas.Type ~ ., scales = "free") + 
   # add title
   labs(title = "Emmissions from motor vehicle sources")
 print(plotObject)
