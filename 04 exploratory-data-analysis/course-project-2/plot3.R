f84bc0f3faec19a97dd7dc98a454c081826e510e
Evgeny Petrovsky <EvgenyPetrovsky@users.noreply.github.com>
2017-01-27 16:50:46 +0100

    use ggplot & connect endpoints

--- 04 exploratory-data-analysis/course-project-2/plot3.R
+++ 04 exploratory-data-analysis/course-project-2/plot3.R
@@ -7,26 +7,36 @@ library(ggplot2)
 NEI <- readRDS("./workdata/input/summarySCC_PM25.rds")
 SCC <- readRDS("./workdata/input/Source_Classification_Code.rds")
 
-# subset data by fips (Baltimore City)
-baltimoreCity <- subset(NEI, fips == "24510")
-
-byYearSource <- 
+allYears <- 
     # converting to tbl_df object of dplyr library and chaining
-    tbl_df(baltimoreCity) %>% 
+    tbl_df(NEI) %>% 
+    # filtering data for Baltimore City by fips = 24510
+    filter(fips == "24510") %>%
     # split data by year
     group_by(year, type) %>%
     # calculate summaries
     summarize(Emissions = sum(Emissions, na.rm = T))
 
+limYears <- allYears %>%
+    # filter by 1999 and 2008 years only
+    filter(year %in% c(1999, 2008))
+  
+
 # open png device
 png("./workdata/output/plot3.png", width = 800, height = 500)
 
 # plotting
-plotObject <- qplot(
-    year, Emissions, data = byYearSource, 
-    geom = "line", facets = . ~ type, 
-    main = "Emmissions by type in Baltimore City by type"
-)
+plotObject <- 
+    # initialize
+    ggplot(allYears, aes(x = year, y = Emissions)) + 
+    # add points for all years
+    geom_point() + 
+    # add lines connecting 1999 and 2008
+    geom_line(data = limYears, aes(x = year, y = Emissions), color = 2, lwd = 1) + 
+    # break plot into facets by emission type
+    facet_grid(. ~ type) + 
+    # add title
+    labs(title = "Emmissions by type in Baltimore City")
 print(plotObject)
 
 # save and close png
